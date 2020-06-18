47a9a15061f1680ca9059f5c14f2af80e4a3cbba
alokKumarSingh89 <itsaloksigh2011@gmail.com>
2020-06-18 16:51:13 +0800

    Config Update

--- clientConfig.js
+++ clientConfig.js
@@ -3,7 +3,7 @@ const AUTH_DOMAIN = "vue-learn.auth0.com";
 const BASE_URL = env
   ? "https://portfolioalok.herokuapp.com"
   : "http://localhost:3000";
-const CLIENT_ID = "Le4qfcLtgn5Tjmwm6LBQEyqvPZLBElj5";
+const CLIENT_ID = "Le4qfcLtsssgn5Tjmwm6LBQEyqvPZLBElj5";
 const NAMESPACE = "https://portfolioalok.herokuapp.com";
 
 module.exports = {
